43bc8060ea8bb7a02ee11f2b0c3b046b35f30a76
Tim Buser <hello@tbuser.dev>
2025-12-25 13:08:15 +0100

    feat: add version flag to display application version and update build process for versioning

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+var Version = "dev"
+
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Printf("photo-renamer version: %s\n", Version)
+		return
+	}
+
 	p := tea.NewProgram(tui.InitialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
